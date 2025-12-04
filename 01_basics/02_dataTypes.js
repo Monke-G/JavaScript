4d55a8479a3cb8c97bf24ebee214291354cc762d
Pranjal <themonkemode@gmail.com>
2025-12-04 17:53:37 +0000

    Learning datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,29 @@
+"use strict";       // treat all JS code as newer version
+
+// alert( 3 + 3)        // we are using nodejs, not browser
+
+console.log(3 
+    + 3) 
+// code readability should be high
+
+console.log("Pranjal")
+
+
+let name = "pranjal"
+let age = 18
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => '' or ""
+// boolean => true/false
+// null => standalone value
+// undefined => variable declared but not assigned a value
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined);       // undefined
+console.log(typeof null);       // object
