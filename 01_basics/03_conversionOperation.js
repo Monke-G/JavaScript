ac97bc0d1e0b834a78a603663ac458df6f27e6f8
Pranjal <themonkemode@gmail.com>
2025-12-14 14:11:15 +0000

    Learning conversion operations

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,28 @@
+let score = null
+// console.log(typeof score);
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN (Not a Number)
+// true => 1; false => 0
+// null => 0
+
+
+let isLoggedIn = "Pranjal"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "Pranjal" => true
+
+
+let somenumber = 33
+
+let stringNumber = String(somenumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
