18c33f54fd4a2a1f42f6ba8426a8ead72a6a7d86
Pranjal Singh Pokhariya <themonkemode@gmail.com>
2025-12-28 14:14:51 +0530

    Learning strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,43 @@
+const name ="Pranjal"
+const repoCount = 5
+// console.log(name + repoCount + " Value");
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('pranjal-hc-com')
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('a'));
+
+const newString = gameName.substring(0,4)
+// console.log(newString);
+
+const anotherString = gameName.slice(-8, 10)
+// console.log(anotherString);
+
+const newStringOne = "   Pranjal   "
+// console.log(newStringOne);
+// console.log(newStringOne.trim());
+
+const url = "https://pranjal.com/pranjal%20singh%20pokhariya"
+// console.log(url.replace('%20', '-'));
+// console.log(url.replaceAll('%20', '-'));
+// console.log(url.includes('sundar'));
+// console.log(gameName.split('-'));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
